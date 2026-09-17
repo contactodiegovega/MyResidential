3fe5cc6c61b3483ba8c6194cf633d577dde74069
contactodiegovega <contactodiegovega@gmail.com>
2026-09-17 13:25:26 +0200

    Update page.js

--- myresidential/app/administrador/page.js
+++ myresidential/app/administrador/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { getConnection } from "../../lib/db";
 import CerrarSesion from "./CerrarSesion";
-
+export const dynamic = "force-dynamic";
 export default async function AdministradorPage() {
   const pool = await getConnection();
 
